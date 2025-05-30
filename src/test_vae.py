88f66c571f83a607fa617b7fb83887b932a27cc2
Kammnoel1 <kammnoel1@gmail.com>
2025-05-30 11:21:35 +0200

    created test script with tensorboard monitoring

--- src/test_vae.py
+++ src/test_vae.py
@@ -1,66 +1,95 @@
+import argparse
 import os
-import glob
+import yaml
 import torch
-import numpy as np
-from src.model.opts.ckpt import load_model
-from src.model.net.modelA import VAEEG
-from src.model.opts.dataset import ClipDataset
 from torch.utils.data import DataLoader
-import torch.nn.functional as F
-from scipy.stats import pearsonr
+from torch.utils.tensorboard import SummaryWriter
+from model.opts.dataset import ClipDataset
+from model.opts.ckpt import init_model
+from model.net.modelA import VAEEG
+from model.net.losses import recon_loss, kl_loss
+from model.opts.viz import batch_imgs
 
-# Set the directory where your checkpoints are saved.
-latest_ckpt = "models/theta_z50/ckpt_epoch_5.ckpt"
+def pearson_index(x, y, dim=-1):
+    xy = x * y
+    xx = x * x
+    yy = y * y
+    mx = x.mean(dim)
+    my = y.mean(dim)
+    mxy = xy.mean(dim)
+    mxx = xx.mean(dim)
+    myy = yy.mean(dim)
+    r = (mxy - mx * my) / torch.sqrt((mxx - mx**2) * (myy - my**2))
+    return r
 
-# Instantiate the model with the same parameters as during training.
-model_params = {
-    "in_channels": 1,
-    "z_dim": 50,  
-    "negative_slope": 0.2,
-    "decoder_last_lstm": False
-}
-model = VAEEG(**model_params)
+def evaluate(model, loader, writer, device):
+    model.eval()
+    total_rec = 0.0
+    total_r = 0.0
+    count = 0
+    with torch.no_grad():
+        for step, x in enumerate(loader, 1):
+            x = x.to(device)
+            mu, log_var, xbar = model(x)
+            rec = recon_loss(x, xbar)
+            pr = pearson_index(x, xbar).mean()
+            total_rec += rec.item()
+            total_r += pr.item()
+            count += 1
+            writer.add_scalar('test/rec_loss', rec, step)
+            writer.add_scalar('test/pearsonr', pr, step)
+            if step == 1:
+                img = batch_imgs(
+                    x.cpu().detach().numpy()[:, 0, :],
+                    xbar.cpu().detach().numpy()[:, 0, :],
+                    256,
+                    4,
+                    2,
+                    fig_size=(8, 5)
+                )
+                writer.add_image('signal', img, step, dataformats='HWC')
 
-# Load the checkpoint 
-aux_info = load_model(model, latest_ckpt)
-model.eval()  
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model.to(device)
+if __name__ == '__main__':
+    p = argparse.ArgumentParser()
+    p.add_argument('--yaml_file', required=True)
+    p.add_argument('--z_dim', type=int, required=True)
+    p.add_argument('--band_name', type=str, required=True)
+    args = p.parse_args()
 
-# Set up your test dataset.
-test_data_dir = "./new_data_downsampled/test/channel_1"  
-band_name = "theta" 
-clip_len = 256
-batch_size = 8
+    # load config
+    with open(args.yaml_file) as f:
+        cfg = yaml.safe_load(f)
+    dataset_params = cfg['DataSet']
+    train_params = cfg['Train']
+    model_params = cfg['Model']
+    model_params['z_dim'] = args.z_dim
+    model_params['name'] = args.band_name
 
-test_dataset = ClipDataset(data_dir=test_data_dir, band_name=band_name, clip_len=clip_len)
-test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, drop_last=False)
+    # paths
+    model_dir = os.path.join(train_params['model_dir'], args.band_name, f"z{args.z_dim}")
+    ckpt_file = os.path.join(model_dir, 'ckpt_epoch_{:d}.ckpt'.format(train_params['n_epoch']))
 
-def compute_pearson(x_np, xhat_np):
-    # Compute Pearson correlation for each sample and average.
-    correlations = []
-    for x, xhat in zip(x_np, xhat_np):
-        # Flatten each sample (they are 1D arrays) 
-        r, _ = pearsonr(x.flatten(), xhat.flatten())
-        correlations.append(r)
-    return np.mean(correlations)
+    # init model
+    model = VAEEG(
+        in_channels=model_params['in_channels'],
+        z_dim=model_params['z_dim'],
+        negative_slope=model_params['negative_slope'],
+        decoder_last_lstm=model_params['decoder_last_lstm']
+    )
+    model, aux, device = init_model(model, n_gpus=train_params["n_gpus"], ckpt_file=ckpt_file)
 
-# Evaluate the model on test data.
-mse_losses, pearson_corrs = [], []
-with torch.no_grad():
-    for batch in test_loader:
-        batch = batch.float()
-        batch = batch.to(device)
-        mu, log_var, xbar = model(batch)
-        mse_loss = F.mse_loss(xbar, batch, reduction="mean")
-        # Gather data on the CPU for Pearson computation.
-        batch_np = batch.cpu().detach().numpy()
-        xbar_np = xbar.cpu().detach().numpy()
-        pearson_val = compute_pearson(batch_np, xbar_np)
-        mse_losses.append(mse_loss.item())
-        pearson_corrs.append(pearson_val)
+    # data loader
+    ds = ClipDataset(
+        data_dir=dataset_params['data_dir'],
+        band_name=model_params['name'],
+        clip_len=dataset_params['clip_len'],
+    )
+    loader = DataLoader(ds, batch_size=dataset_params['batch_size'], num_workers=dataset_params['num_workers'], shuffle=False)
 
-avg_mse = np.mean(mse_losses)
-avg_pearson = np.mean(pearson_corrs)
-print(f"Average Reconstruction MSE on Test Data: {avg_mse}")
-print(f"Average Pearson Correlation on Test Data: {avg_pearson}")
+    # tensorboard writer
+    log_dir = os.path.join(model_dir, 'test_logs')
+    writer = SummaryWriter(log_dir)
+
+    # run evaluation
+    evaluate(model, loader, writer, device)
+    writer.close()
